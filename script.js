13c7385ab93008e0cd191aae50e089238b96000c
pinqwt <pinqwt@gmail.com>
2026-05-28 00:51:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -338,9 +338,7 @@ function renderProducts() {
     const liveQty    = getLiveQty(p.id);
     const isOut      = liveStatus === 'out-of-stock';
     const isLow      = liveStatus === 'low-stock';
-    const photo      = customPhotos[p.id]
-                     || (p.img && !p.img.startsWith('product-images/') ? p.img : '')
-                     || `https://picsum.photos/seed/dhow${p.id}/400/320`;
+    const photo      = customPhotos[p.id] || p.img;
     // Arabic product name/desc
     const arP = isAr && _AR_PRODUCTS[p.id];
     const pName = arP ? arP.name : p.name;
